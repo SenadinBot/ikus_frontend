6cc193c6a97add6d0f448b678ecaad330c082d53
senadin <senadin.b@maus.ba>
2021-06-22 16:47:25 +0200

    feat: init commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// compile scss into css
+function style() {
+    // return gulp.src('./scss/**/*.scss')
+
+    // 1. where is scss file
+    return gulp.src('./scss/style.scss')
+        // 2. pass that file through sass compiler
+        .pipe(sass().on('error', sass.logError))
+        // 3. where do I save the compiled CSS?
+        .pipe(gulp.dest('./css'))
+        // 4. stream changes to all browser
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
