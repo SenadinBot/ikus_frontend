c586c769fb284948fc6ee2b5a0e532cab74d7688
senadin <senadin.b@maus.ba>
2021-06-27 18:55:46 +0200

    feat: image gallery

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
 $(function () {
+
+    $('.filtr-container').filterizr({
+        animationDuration: 0.3,
+        delayMode: 'progressive',
+        easing: 'ease-out',
+        filter: 'all',
+    });
+
+    $('.filter-nav li').on('click', function () {
+        $('.filter-nav li').removeClass('active');
+        $(this).addClass('active');
+        var filter = $(this).data('fltr');
+    });
+
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
 })
 
 $(".counter").counter();
